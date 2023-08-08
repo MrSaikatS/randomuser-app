ec15a4d0570af5f0983f7ab5d989e0317025293a
Saikat Sardar <saikatsardar5@gmail.com>
2023-08-08 16:33:02 +0530

    structure

--- components/Display.tsx
+++ components/Display.tsx
@@ -1,8 +1,12 @@
+import UserCard from "./UserCard";
+
 const Display = () => {
   return (
     <>
       <div className='flex justify-center items-center h-[86dvh]'>
-        <div className='px-6 bg-slate-400 max-w-md'>Page Content</div>
+        <div className=''>
+          <UserCard />
+        </div>
       </div>
     </>
   );
--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,10 +1,57 @@
 // Explicitly use import type
+import { Avatar, Card, CardBody, Divider } from "@nextui-org/react";
+import {
+  UsersIcon,
+  ClockIcon,
+  InboxIcon,
+  MapPinIcon,
+  GlobeAsiaAustraliaIcon,
+} from "@heroicons/react/24/solid";
 import type { UserType } from "../utils/UserType";
 
 const UserCard = () => {
   return (
     <>
-      <div className=''></div>
+      <Card className='max-w-sm m-2'>
+        <CardBody>
+          <div className='flex flex-col items-center justify-center gap-2 pb-5'>
+            <Avatar
+              src='https://i.pravatar.cc/150?u=a04258114e29026708c'
+              className='w-20 h-20 text-large'
+            />
+            <span className='text-4xl font-bold'>Mr Evan Nguyen</span>
+          </div>
+
+          <Divider />
+
+          <div className='pt-5 space-y-4 w-[19rem] font-bold'>
+            <div className='flex items-center gap-4'>
+              <UsersIcon className='h-7 w-7' />
+              <span className='text-xl'>Male</span>
+            </div>
+
+            <div className='flex items-center gap-4'>
+              <ClockIcon className='h-7 w-7' />
+              <span className='text-xl'>35 years</span>
+            </div>
+
+            <div className='flex items-center gap-4'>
+              <InboxIcon className='h-7 w-7' />
+              <span className='text-xl'>evan.nguyen@example.com</span>
+            </div>
+
+            <div className='flex items-center gap-4'>
+              <MapPinIcon className='h-7 w-7' />
+              <span className='text-xl'>city , state</span>
+            </div>
+
+            <div className='flex items-center gap-4'>
+              <GlobeAsiaAustraliaIcon className='h-7 w-7' />
+              <span className='text-xl'>country</span>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
     </>
   );
 };
